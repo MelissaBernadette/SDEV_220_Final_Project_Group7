68ea2306f2b822fb68012b7698483abe60b5b048
Jaecobd1 <jaecob.d1@gmail.com>
2022-07-27 16:09:07 -0400

    updated /products/# page

--- src/products/views.py
+++ src/products/views.py
@@ -9,14 +9,20 @@ from .decorators import auth_users, allowed_users
 # Create your views here.
 
 
-def product_detail_view(request):
+def product_detail_view(request, pk):
     obj = Product.objects.get(id=1)
     context = {
-        'title': obj.title,
+        "title": obj.title,
+        "price": obj.price,
+        "color": obj.color,
+        "shape": obj.shape,
+        "material": obj.material,
+        "size": obj.size,
     }
     return render(request, "product/detail.html", context)
 
-@login_required(login_url='user-login')
+
+@login_required(login_url="user-login")
 def index(request):
     product = Product.objects.all()
     product_count = product.count()
@@ -27,28 +33,28 @@ def index(request):
     vendor = Vendor.objects.all()
     vendor_count = vendor.count()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
             obj = form.save(commit=False)
             obj.customer = request.user
             obj.save()
-            return redirect('dashboard-index')
+            return redirect("dashboard-index")
     else:
         form = OrderForm()
     context = {
-        'form': form,
-        'order': order,
-        'product': product,
-        'product_count': product_count,
-        'order_count': order_count,
-        'customer_count': customer_count,
-        'vendor_count': vendor_count,
+        "form": form,
+        "order": order,
+        "product": product,
+        "product_count": product_count,
+        "order_count": order_count,
+        "customer_count": customer_count,
+        "vendor_count": vendor_count,
     }
-    return render(request, 'dashboard/index.html', context)
+    return render(request, "dashboard/index.html", context)
 
 
-@login_required(login_url='user-login')
+@login_required(login_url="user-login")
 def products(request):
     product = Product.objects.all()
     product_count = product.count()
@@ -58,37 +64,35 @@ def products(request):
     order_count = order.count()
     vendor = Vendor.objects.all()
     vendor_count = vendor.count()
-    product_quantity = Product.objects.filter(title='')
-    if request.method == 'POST':
+    product_quantity = Product.objects.filter(title="")
+    if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            product_title = form.cleaned_data.get('title')
-            messages.success(request, f'{product_title} has been added')
-            return redirect('dashboard-products')
+            product_title = form.cleaned_data.get("title")
+            messages.success(request, f"{product_title} has been added")
+            return redirect("dashboard-products")
     else:
         form = ProductForm()
     context = {
-        'product': product,
-        'form': form,
-        'customer_count': customer_count,
-        'product_count': product_count,
-        'order_count': order_count,
-        'vendor_count': vendor_count,
+        "product": product,
+        "form": form,
+        "customer_count": customer_count,
+        "product_count": product_count,
+        "order_count": order_count,
+        "vendor_count": vendor_count,
     }
-    return render(request, 'dashboard/products.html', context)
+    return render(request, "dashboard/products.html", context)
 
 
-@login_required(login_url='user-login')
+@login_required(login_url="user-login")
 def product_detail(request, pk):
-    context = {
-
-    }
-    return render(request, 'dashboard/products_detail.html', context)
+    context = {}
+    return render(request, "dashboard/products_detail.html", context)
 
 
-@login_required(login_url='user-login')
-@allowed_users(allowed_roles=['Admin'])
+@login_required(login_url="user-login")
+@allowed_users(allowed_roles=["Admin"])
 def customers(request):
     customer = User.objects.filter(groups=2)
     customer_count = customer.count()
@@ -97,16 +101,16 @@ def customers(request):
     order = Order.objects.all()
     order_count = order.count()
     context = {
-        'customer': customer,
-        'customer_count': customer_count,
-        'product_count': product_count,
-        'order_count': order_count,
+        "customer": customer,
+        "customer_count": customer_count,
+        "product_count": product_count,
+        "order_count": order_count,
     }
-    return render(request, 'dashboard/customers.html', context)
+    return render(request, "dashboard/customers.html", context)
 
 
-@login_required(login_url='user-login')
-@allowed_users(allowed_roles=['Admin'])
+@login_required(login_url="user-login")
+@allowed_users(allowed_roles=["Admin"])
 def customer_detail(request, pk):
     customer = User.objects.filter(groups=2)
     customer_count = customer.count()
@@ -116,46 +120,43 @@ def customer_detail(request, pk):
     order_count = order.count()
     customers = User.objects.get(id=pk)
     context = {
-        'customers': customers,
-        'customer_count': customer_count,
-        'product_count': product_count,
-        'order_count': order_count,
-
+        "customers": customers,
+        "customer_count": customer_count,
+        "product_count": product_count,
+        "order_count": order_count,
     }
-    return render(request, 'dashboard/customers_detail.html', context)
+    return render(request, "dashboard/customers_detail.html", context)
 
 
-@login_required(login_url='user-login')
-@allowed_users(allowed_roles=['Admin'])
+@login_required(login_url="user-login")
+@allowed_users(allowed_roles=["Admin"])
 def product_edit(request, pk):
     item = Product.objects.get(id=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProductForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
-            return redirect('dashboard-products')
+            return redirect("dashboard-products")
     else:
         form = ProductForm(instance=item)
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'dashboard/products_edit.html', context)
+    return render(request, "dashboard/products_edit.html", context)
 
 
-@login_required(login_url='user-login')
-@allowed_users(allowed_roles=['Admin'])
+@login_required(login_url="user-login")
+@allowed_users(allowed_roles=["Admin"])
 def product_delete(request, pk):
     item = Product.objects.get(id=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         item.delete()
-        return redirect('dashboard-products')
-    context = {
-        'item': item
-    }
-    return render(request, 'dashboard/products_delete.html', context)
+        return redirect("dashboard-products")
+    context = {"item": item}
+    return render(request, "dashboard/products_delete.html", context)
 
 
-@login_required(login_url='user-login')
+@login_required(login_url="user-login")
 def order(request):
     order = Order.objects.all()
     order_count = order.count()
@@ -165,9 +166,9 @@ def order(request):
     product_count = product.count()
 
     context = {
-        'order': order,
-        'customer_count': customer_count,
-        'product_count': product_count,
-        'order_count': order_count,
+        "order": order,
+        "customer_count": customer_count,
+        "product_count": product_count,
+        "order_count": order_count,
     }
-    return render(request, 'dashboard/order.html', context)
+    return render(request, "dashboard/order.html", context)
